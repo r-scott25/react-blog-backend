a5eb30f03458f7d925e196d5dfa87b48e794c101
r-scott25 <rachelbernal2@gmail.com>
2022-12-05 15:32:25 -0600

    set up postman

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
-app.get('/hello', (req, res) => {
-  res.send('Hello!');
+app.post('/hello', (req, res) => {
+  console.log(req.body);
+  res.send(`Hello ${req.body.name}!`);
 });
 
 app.listen(8000, () => {
