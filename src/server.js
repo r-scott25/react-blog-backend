e1439edf1598bdfe9af3e8f9d5fccebaa0806651
r-scott25 <rachelbernal2@gmail.com>
2022-12-05 16:19:08 -0600

    set up nodemon

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.put('/api/articles/:name/upvote', (req, res) => {
   const article = articlesInfo.find(a => a.name === name);
   if (article) {
     article.upvotes += 1;
-    res.send(`The ${name} article now has ${article.upvotes} upvotes`)
+    res.send(`The ${name} article now has ${article.upvotes} upvotes!!!`)
   } else {
     res.send('That article doesn\'t exist');
   }
